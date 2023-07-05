a4678496edcb1c4c26d42fbf672d977460123c96
dalia-alawneh <alawnehdalia340@gmail.com>
2023-07-05 10:28:54 +0200

    Delete useless comments

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,8 +7,8 @@ async function getRandomAdvice() {
         ADVICE_CARD.children[1].textContent = "Loading .. "
         let response = await fetch(API_URL);
         if (response.ok) {
-            let { slip } = await response.json(); // Destructure the slip object and assign it to adviceSlip
-            adviceSlip = slip; // Assign the destructured adviceSlip to the global slip variable
+            let { slip } = await response.json();
+            adviceSlip = slip; 
             console.log(slip);
             display();
         } else {
