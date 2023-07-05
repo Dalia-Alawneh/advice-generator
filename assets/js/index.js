f798952093a67288c9b5c0dc95fe8a7a58f82ad4
dalia-alawneh <alawnehdalia340@gmail.com>
2023-07-05 10:32:45 +0200

    Add dice btn functionality

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,7 @@
 const API_URL = "https://api.adviceslip.com/advice";
 let adviceSlip = {};
 const ADVICE_CARD = document.querySelector('.card');
-
+const DICE_BTN = document.querySelector('.dice')
 async function getRandomAdvice() {
     try {
         ADVICE_CARD.children[1].textContent = "Loading .. "
@@ -25,3 +25,7 @@ function display() {
     ADVICE_CARD.children[0].firstElementChild.textContent = adviceSlip.id;
     ADVICE_CARD.children[1].textContent = adviceSlip.advice;
 }
+
+DICE_BTN.addEventListener('click', ()=>{
+    getRandomAdvice()
+})
